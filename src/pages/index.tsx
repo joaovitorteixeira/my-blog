e44a2f2079faeee69c2574a61d196a6349578642
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2025-01-21 20:26:43 +1000

    feat: add h1 tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,23 +5,22 @@ import styles from "./index.module.css";
 import AboutMe from "@site/src/components/AboutMe";
 import TechSkill from "@site/src/components/TechSkill";
 
-
 function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <img src="img/profile.jpg" alt="João" className={styles.profileImage} />
       <div className={styles.textContent}>
-        <p className={styles.introText}>
-          Hello 👋 My name is{" "}
-          <a
-            href="https://youtu.be/LH1j-x3D85E?si=pYwKLut1RnYfoVqE&t=18"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.nameLink}
-          >
-            João
-          </a>
-        </p>
+          <h1 className={styles.introText}>
+            Hello 👋 My name is{" "}
+            <a
+              href="https://youtu.be/LH1j-x3D85E?si=pYwKLut1RnYfoVqE&t=18"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.nameLink}
+            >
+              João
+            </a>
+          </h1>
       </div>
     </header>
   );
